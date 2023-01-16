e06237094f4a86bb094a592e9e35ba69ebe9868d
William Cahill <drewlikesramen@gmail.com>
2023-01-15 20:59:40 -0500

    Improving colors

--- src/class_helpers.rs
+++ src/class_helpers.rs
@@ -1,4 +1,4 @@
-use bevy_ui::{Val, Size, UiRect};
+use bevy_ui::{Val, Size, UiRect, BackgroundColor};
 use bevy_render::color::Color;
 
 pub fn size(width: Val, height: Val) -> Size {
@@ -45,20 +45,20 @@ pub fn px(num: impl Tof32) -> Val {
     Val::Px(num.to_f32())
 }
 
-pub fn rgb8(r: u8, g: u8, b: u8) -> Color {
-    Color::rgb_u8(r, g, b)
+pub fn rgb8(r: u8, g: u8, b: u8) -> BackgroundColor {
+    Color::rgb_u8(r, g, b).into()
 }
 
-pub fn rgba8(r: u8, g: u8, b: u8, a: u8) -> Color {
-    Color::rgba_u8(r, g, b, a)
+pub fn rgba8(r: u8, g: u8, b: u8, a: u8) -> BackgroundColor {
+    Color::rgba_u8(r, g, b, a).into()
 }
 
-pub fn rgb(r: f32, g: f32, b: f32) -> Color {
-    Color::rgb(r, g, b)
+pub fn rgb(r: f32, g: f32, b: f32) -> BackgroundColor {
+    Color::rgb(r, g, b).into()
 }
 
-pub fn rgba(r: f32, g: f32, b: f32, a: f32) -> Color {
-    Color::rgba(r, g, b, a)
+pub fn rgba(r: f32, g: f32, b: f32, a: f32) -> BackgroundColor {
+    Color::rgba(r, g, b, a).into()
 }
 
 pub const fn auto() -> Val {
@@ -143,4 +143,49 @@ impl Tof32 for u128 {
     fn to_f32(self) -> f32 {
         self as f32
     }
+}
+
+/// Includes const background colors
+pub mod color {
+    use bevy_render::color::Color;
+    use bevy_ui::BackgroundColor;
+
+    pub const ALICE_BLUE: BackgroundColor = BackgroundColor(Color::rgb(0.94, 0.97, 1.0));
+    pub const ANTIQUE_WHITE: BackgroundColor = BackgroundColor(Color::rgb(0.98, 0.92, 0.84));
+    pub const AQUAMARINE: BackgroundColor = BackgroundColor(Color::rgb(0.49, 1.0, 0.83));
+    pub const AZURE: BackgroundColor = BackgroundColor(Color::rgb(0.94, 1.0, 1.0));
+    pub const BEIGE: BackgroundColor = BackgroundColor(Color::rgb(0.96, 0.96, 0.86));
+    pub const BISQUE: BackgroundColor = BackgroundColor(Color::rgb(1.0, 0.89, 0.77));
+    pub const BLACK: BackgroundColor = BackgroundColor(Color::rgb(0.0, 0.0, 0.0));
+    pub const BLUE: BackgroundColor = BackgroundColor(Color::rgb(0.0, 0.0, 1.0));
+    pub const CRIMSON: BackgroundColor = BackgroundColor(Color::rgb(0.86, 0.08, 0.24));
+    pub const CYAN: BackgroundColor = BackgroundColor(Color::rgb(0.0, 1.0, 1.0));
+    pub const DARK_GRAY: BackgroundColor = BackgroundColor(Color::rgb(0.25, 0.25, 0.25));
+    pub const DARK_GREEN: BackgroundColor = BackgroundColor(Color::rgb(0.0, 0.5, 0.0));
+    pub const FUCHSIA: BackgroundColor = BackgroundColor(Color::rgb(1.0, 0.0, 1.0));
+    pub const GOLD: BackgroundColor = BackgroundColor(Color::rgb(1.0, 0.84, 0.0));
+    pub const GRAY: BackgroundColor = BackgroundColor(Color::rgb(0.5, 0.5, 0.5));
+    pub const GREEN: BackgroundColor = BackgroundColor(Color::rgb(0.0, 1.0, 0.0));
+    pub const INDIGO: BackgroundColor = BackgroundColor(Color::rgb(0.29, 0.0, 0.51));
+    pub const LIME_GREEN: BackgroundColor = BackgroundColor(Color::rgb(0.2, 0.8, 0.2));
+    pub const MAROON: BackgroundColor = BackgroundColor(Color::rgb(0.5, 0.0, 0.0));
+    pub const MIDNIGHT_BLUE: BackgroundColor = BackgroundColor(Color::rgb(0.1, 0.1, 0.44));
+    pub const NAVY: BackgroundColor = BackgroundColor(Color::rgb(0.0, 0.0, 0.5));
+    pub const NONE: BackgroundColor = BackgroundColor(Color::rgba(0.0, 0.0, 0.0, 0.0));
+    pub const OLIVE: BackgroundColor = BackgroundColor(Color::rgb(0.5, 0.5, 0.0));
+    pub const ORANGE: BackgroundColor = BackgroundColor(Color::rgb(1.0, 0.65, 0.0));
+    pub const ORANGE_RED: BackgroundColor = BackgroundColor(Color::rgb(1.0, 0.27, 0.0));
+    pub const PINK: BackgroundColor = BackgroundColor(Color::rgb(1.0, 0.08, 0.58));
+    pub const PURPLE: BackgroundColor = BackgroundColor(Color::rgb(0.5, 0.0, 0.5));
+    pub const RED: BackgroundColor = BackgroundColor(Color::rgb(1.0, 0.0, 0.0));
+    pub const SALMON: BackgroundColor = BackgroundColor(Color::rgb(0.98, 0.5, 0.45));
+    pub const SEA_GREEN: BackgroundColor = BackgroundColor(Color::rgb(0.18, 0.55, 0.34));
+    pub const SILVER: BackgroundColor = BackgroundColor(Color::rgb(0.75, 0.75, 0.75));
+    pub const TEAL: BackgroundColor = BackgroundColor(Color::rgb(0.0, 0.5, 0.5));
+    pub const TOMATO: BackgroundColor = BackgroundColor(Color::rgb(1.0, 0.39, 0.28));
+    pub const TURQUOISE: BackgroundColor = BackgroundColor(Color::rgb(0.25, 0.88, 0.82));
+    pub const VIOLET: BackgroundColor = BackgroundColor(Color::rgb(0.93, 0.51, 0.93));
+    pub const WHITE: BackgroundColor = BackgroundColor(Color::rgb(1.0, 1.0, 1.0));
+    pub const YELLOW: BackgroundColor = BackgroundColor(Color::rgb(1.0, 1.0, 0.0));
+    pub const YELLOW_GREEN: BackgroundColor = BackgroundColor(Color::rgb(0.6, 0.8, 0.2));
 }
