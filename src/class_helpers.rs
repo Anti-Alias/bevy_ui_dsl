01e71ebf9a3761a8e7f097ee67741ceaf91bcc0f
William Cahill <drewlikesramen@gmail.com>
2023-01-15 21:05:18 -0500

    Updating colors

--- src/class_helpers.rs
+++ src/class_helpers.rs
@@ -146,7 +146,7 @@ impl Tof32 for u128 {
 }
 
 /// Includes const background colors
-pub mod color {
+pub mod bcolor {
     use bevy_render::color::Color;
     use bevy_ui::BackgroundColor;
 
@@ -188,4 +188,48 @@ pub mod color {
     pub const WHITE: BackgroundColor = BackgroundColor(Color::rgb(1.0, 1.0, 1.0));
     pub const YELLOW: BackgroundColor = BackgroundColor(Color::rgb(1.0, 1.0, 0.0));
     pub const YELLOW_GREEN: BackgroundColor = BackgroundColor(Color::rgb(0.6, 0.8, 0.2));
+}
+
+/// Includes const colors
+pub mod color {
+    use bevy_render::color::Color;
+
+    pub const ALICE_BLUE: Color = Color::rgb(0.94, 0.97, 1.0);
+    pub const ANTIQUE_WHITE: Color = Color::rgb(0.98, 0.92, 0.84);
+    pub const AQUAMARINE: Color = Color::rgb(0.49, 1.0, 0.83);
+    pub const AZURE: Color = Color::rgb(0.94, 1.0, 1.0);
+    pub const BEIGE: Color = Color::rgb(0.96, 0.96, 0.86);
+    pub const BISQUE: Color = Color::rgb(1.0, 0.89, 0.77);
+    pub const BLACK: Color = Color::rgb(0.0, 0.0, 0.0);
+    pub const BLUE: Color = Color::rgb(0.0, 0.0, 1.0);
+    pub const CRIMSON: Color = Color::rgb(0.86, 0.08, 0.24);
+    pub const CYAN: Color = Color::rgb(0.0, 1.0, 1.0);
+    pub const DARK_GRAY: Color = Color::rgb(0.25, 0.25, 0.25);
+    pub const DARK_GREEN: Color = Color::rgb(0.0, 0.5, 0.0);
+    pub const FUCHSIA: Color = Color::rgb(1.0, 0.0, 1.0);
+    pub const GOLD: Color = Color::rgb(1.0, 0.84, 0.0);
+    pub const GRAY: Color = Color::rgb(0.5, 0.5, 0.5);
+    pub const GREEN: Color = Color::rgb(0.0, 1.0, 0.0);
+    pub const INDIGO: Color = Color::rgb(0.29, 0.0, 0.51);
+    pub const LIME_GREEN: Color = Color::rgb(0.2, 0.8, 0.2);
+    pub const MAROON: Color = Color::rgb(0.5, 0.0, 0.0);
+    pub const MIDNIGHT_BLUE: Color = Color::rgb(0.1, 0.1, 0.44);
+    pub const NAVY: Color = Color::rgb(0.0, 0.0, 0.5);
+    pub const NONE: Color = Color::rgba(0.0, 0.0, 0.0, 0.0);
+    pub const OLIVE: Color = Color::rgb(0.5, 0.5, 0.0);
+    pub const ORANGE: Color = Color::rgb(1.0, 0.65, 0.0);
+    pub const ORANGE_RED: Color = Color::rgb(1.0, 0.27, 0.0);
+    pub const PINK: Color = Color::rgb(1.0, 0.08, 0.58);
+    pub const PURPLE: Color = Color::rgb(0.5, 0.0, 0.5);
+    pub const RED: Color = Color::rgb(1.0, 0.0, 0.0);
+    pub const SALMON: Color = Color::rgb(0.98, 0.5, 0.45);
+    pub const SEA_GREEN: Color = Color::rgb(0.18, 0.55, 0.34);
+    pub const SILVER: Color = Color::rgb(0.75, 0.75, 0.75);
+    pub const TEAL: Color = Color::rgb(0.0, 0.5, 0.5);
+    pub const TOMATO: Color = Color::rgb(1.0, 0.39, 0.28);
+    pub const TURQUOISE: Color = Color::rgb(0.25, 0.88, 0.82);
+    pub const VIOLET: Color = Color::rgb(0.93, 0.51, 0.93);
+    pub const WHITE: Color = Color::rgb(1.0, 1.0, 1.0);
+    pub const YELLOW: Color = Color::rgb(1.0, 1.0, 0.0);
+    pub const YELLOW_GREEN: Color = Color::rgb(0.6, 0.8, 0.2);
 }
